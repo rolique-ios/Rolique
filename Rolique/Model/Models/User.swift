b4b83aefeaede14a422b1d8f3633a7ca95100ecf
Maksym Ivanyk <maksym.ivanyk@rolique.io>
2019-08-15 10:39:24 +0300

    Implement CoreData, add managed User and SlackProfile entities

--- Rolique/Model/Models/User.swift
+++ Rolique/Model/Models/User.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UsersWidget
+import CoreData
 
 public final class User: Codable {
   
@@ -40,7 +41,51 @@ public final class User: Codable {
     self.roles = roles
     self.vacationData = vacationData
   }
+  
+  func saveToCoreData(context: NSManagedObjectContext? = nil) {
+    let context = context ?? CoreDataManager.shared.persistentContainer.viewContext
+    createOrUpdate(with: context)
+    try? CoreDataManager.shared.saveToCoreData(context: context)
+  }
+  
+  @discardableResult
+  func createOrUpdate(with context: NSManagedObjectContext? = nil) -> ManagedUser? {
+    let context = context ?? CoreDataManager.shared.persistentContainer.viewContext
+    
+    guard let userEntityDescription = NSEntityDescription.entity(forEntityName: "ManagedUser", in: context) else { return nil }
+    
+    let managedUser = CoreDataManager.shared.getManagedObject(predicate: NSPredicate(format: "id == %@", self.id), context: context).first ?? ManagedUser(entity: userEntityDescription, insertInto: context)
+    managedUser.id = self.id
+    managedUser.slackProfile = self.slackProfile.createOrUpdate(with: context)
+    managedUser.birthday = self.birthday
+    managedUser.dateOfJoining = self.dateOfJoining
+    managedUser.eduPoints = self.eduPoints ?? 0
+    managedUser.emergencyDays = self.emergencyDays ?? 0
+    managedUser.roles = self.roles
+    managedUser.vacationData = self.vacationData
+    
+    return managedUser
+  }
+  
+  static func getFromCoreData(with id: String, context: NSManagedObjectContext? = nil) -> User? {
+    let context = context ?? CoreDataManager.shared.persistentContainer.viewContext
 
+    guard let managedUser: ManagedUser = CoreDataManager.shared.getManagedObject(predicate: NSPredicate(format: "id == %@", id), context: context).first,
+      let realName = managedUser.slackProfile?.realName,
+      let slackProfile = SlackProfile.getFromCoreData(with: realName, context: context)
+      else { return nil }
+
+    let user = User(id: id,
+                    slackProfile: slackProfile,
+                    birthday: managedUser.birthday,
+                    dateOfJoining: managedUser.dateOfJoining,
+                    eduPoints: managedUser.eduPoints,
+                    emergencyDays: managedUser.emergencyDays,
+                    roles: managedUser.roles ?? [],
+                    vacationData: managedUser.vacationData)
+
+    return user
+  }
 }
 
 // MARK: - SlackProfile
@@ -118,6 +163,102 @@ public final class SlackProfile: Codable {
     self.statusTextCanonical = statusTextCanonical
     self.team = team
     self.title = title
+  }
+  
+  func saveToCoreData(context: NSManagedObjectContext? = nil) {
+    let context = context ?? CoreDataManager.shared.persistentContainer.viewContext
+    createOrUpdate(with: context)
+    try? CoreDataManager.shared.saveToCoreData(context: context)
+  }
+  
+  @discardableResult
+  func createOrUpdate(with context: NSManagedObjectContext? = nil) -> ManagedSlackProfile? {
+    let context = context ?? CoreDataManager.shared.persistentContainer.viewContext
+    
+    guard let slackProfileEntityRequest = NSEntityDescription.entity(forEntityName: "ManagedSlackProfile", in: context) else { return nil }
+    
+    let managedSlackProfile = CoreDataManager.shared.getManagedObject(predicate: NSPredicate(format: "realName == %@", self.realName),
+                                                                      context: context).first ?? ManagedSlackProfile(entity: slackProfileEntityRequest, insertInto: context)
+    managedSlackProfile.avatarHash = self.avatarHash
+    managedSlackProfile.displayName = self.displayName
+    managedSlackProfile.displayNameNormalized = self.displayNameNormalized
+    managedSlackProfile.email = self.email
+    managedSlackProfile.firstName = self.firstName
+    managedSlackProfile.image24 = self.image24
+    managedSlackProfile.image32 = self.image32
+    managedSlackProfile.image48 = self.image48
+    managedSlackProfile.image72 = self.image72
+    managedSlackProfile.image192 = self.image192
+    managedSlackProfile.image512 = self.image512
+    managedSlackProfile.image1024 = self.image1024
+    managedSlackProfile.imageOriginal = self.imageOriginal
+    managedSlackProfile.isCustomImage = self.isCustomImage ?? false
+    managedSlackProfile.lastName = self.lastName
+    managedSlackProfile.phone = self.phone
+    managedSlackProfile.realName = self.realName
+    managedSlackProfile.realNameNormalized = self.realNameNormalized
+    managedSlackProfile.skype = self.skype
+    managedSlackProfile.statusEmoji = self.statusEmoji
+    managedSlackProfile.statusExpiration = Int64(self.statusExpiration)
+    managedSlackProfile.statusText = self.statusText
+    managedSlackProfile.statusTextCanonical = self.statusTextCanonical
+    managedSlackProfile.team = self.team
+    managedSlackProfile.title = self.title
+    
+    return managedSlackProfile
+  }
+  
+  static func getFromCoreData(with realName: String, context: NSManagedObjectContext? = nil) -> SlackProfile? {
+    let context = context ?? CoreDataManager.shared.persistentContainer.viewContext
+
+    guard let managedSlackProfile: ManagedSlackProfile = CoreDataManager.shared.getManagedObject(predicate: NSPredicate(format: "realName == %@", realName), context: context).first,
+      let avatarHash = managedSlackProfile.avatarHash,
+      let displayName = managedSlackProfile.displayName,
+      let displayNameNormalized = managedSlackProfile.displayNameNormalized,
+      let email = managedSlackProfile.email,
+      let image192 = managedSlackProfile.image192,
+      let image24 = managedSlackProfile.image24,
+      let image32 = managedSlackProfile.image32,
+      let image48 = managedSlackProfile.image48,
+      let image512 = managedSlackProfile.image512,
+      let image72 = managedSlackProfile.image72,
+      let phone = managedSlackProfile.phone,
+      let realNameNormalized = managedSlackProfile.realNameNormalized,
+      let skype = managedSlackProfile.skype,
+      let statusEmoji = managedSlackProfile.statusEmoji,
+      let statusText = managedSlackProfile.statusText,
+      let statusTextCanonical = managedSlackProfile.statusTextCanonical,
+      let team = managedSlackProfile.team,
+      let title = managedSlackProfile.title
+      else { return nil }
+    
+    let slackProfile = SlackProfile(avatarHash: avatarHash,
+                                    displayName: displayName,
+                                    displayNameNormalized: displayNameNormalized,
+                                    email: email,
+                                    firstName: managedSlackProfile.firstName,
+                                    image1024: managedSlackProfile.image1024,
+                                    image192: image192,
+                                    image24: image24,
+                                    image32: image32,
+                                    image48: image48,
+                                    image512: image512,
+                                    image72: image72,
+                                    imageOriginal: managedSlackProfile.imageOriginal,
+                                    isCustomImage: managedSlackProfile.isCustomImage,
+                                    lastName: managedSlackProfile.lastName,
+                                    phone: phone,
+                                    realName: realName,
+                                    realNameNormalized: realNameNormalized,
+                                    skype: skype,
+                                    statusEmoji: statusEmoji,
+                                    statusExpiration: Int(managedSlackProfile.statusExpiration),
+                                    statusText: statusText,
+                                    statusTextCanonical: statusTextCanonical,
+                                    team: team,
+                                    title: title)
+
+    return slackProfile
   }
 }
 
